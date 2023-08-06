5edeb2b5f622b9075a202d2089fd3ef283c829e9
Dinesh R <82563408+Dinesh007U@users.noreply.github.com>
2023-08-06 11:01:53 +0530

    Add files via upload
    Completed Exception code

---
+++ customer/src/main/java/com/application/customer/service/impl/CustomValidationMethods.java
@@ -0,0 +1,65 @@
+package com.application.customer.service.impl;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.validation.ValidationException;
+
+import java.time.LocalDate;
+import java.util.regex.Pattern;
+
+@Component
+public class CustomValidationMethods {
+
+    public void validateName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new ValidationException("Name cannot be blank.");
+        }
+    }
+    
+    public void validateEmail(String email) {
+    	//email validation:
+    	 String emailRegex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+         Pattern pattern = Pattern.compile(emailRegex);
+
+         if (email == null || email.trim().isEmpty() || !pattern.matcher(email).matches()) {
+             throw new ValidationException("Invalid email format");
+         }
+     
+    }
+
+    public void validateAge(int age) {
+        if (age < 0 || age > 150) {
+            throw new ValidationException("Age should have a range of 0 to 150 values.");
+        }
+    }
+
+    public void validateCity(String city) {
+        if (city == null || city.trim().isEmpty()) {
+            throw new ValidationException("City cannot be blank.");
+        }
+    }
+
+    public void validateState(String state) {
+        if (state == null || state.trim().isEmpty()) {
+            throw new ValidationException("State cannot be blank.");
+        }
+    }
+
+    public void validateCountry(String country) {
+        if (country == null || country.trim().isEmpty()) {
+            throw new ValidationException("Country cannot be blank.");
+        }
+    }
+
+    public void validateZipcode(Integer zipcode) {
+    	 if (zipcode == null || String.valueOf(zipcode).length() < 6) {
+    		 throw new ValidationException("Zipcode must be at least 6 characters.");
+        }
+    }
+
+    public void validateOnboardedDate(LocalDate onboardedDate) {
+        if (onboardedDate == null || onboardedDate.isAfter(LocalDate.now())) {
+            throw new ValidationException("OnboardedDate should be current date or before the current date.");
+        }
+    }
+}
--- customer/src/main/java/com/application/customer/service/impl/CustomerServiceImple.java
+++ customer/src/main/java/com/application/customer/service/impl/CustomerServiceImple.java
@@ -3,21 +3,27 @@ package com.application.customer.service.impl;
 import java.sql.Date;
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
-
-
+import com.application.customer.exception.CustomerNotFoundException;
+import com.application.customer.exception.DuplicateConflictException;
 import com.application.customer.model.Customer;
 import com.application.customer.model.CustomerRepository;
+import com.application.customer.model.SearchCriteria;
 import com.application.customer.service.CustomerService;
 
 @Service
+@Validated
 public class CustomerServiceImple implements CustomerService {
 
     CustomerRepository customerRepository;
+	CustomValidationMethods customValidationMethods;
 
-    public CustomerServiceImple(CustomerRepository customerRepository) {
+    public CustomerServiceImple(CustomerRepository customerRepository,CustomValidationMethods customValidationMethods) {
         this.customerRepository = customerRepository;
+        this.customValidationMethods = customValidationMethods;
     }
     
     @Override
@@ -27,27 +33,83 @@ public class CustomerServiceImple implements CustomerService {
 
 	@Override
 	public Customer getCustomer(String id) {
-    	return customerRepository.findById(id).get();
+	    return customerRepository.findById(id)
+        .orElseThrow(() -> new CustomerNotFoundException("Customer with ID " + id + " not found"));
+
 		}
 	@Override
 	public String createCustomer(Customer customer) {
-		customerRepository.save(customer);
-		return "Success";
-	}
+
+		 String id = customer.getId();
+	        if (customerRepository.existsById(id)) {
+	            // Customer with the given ID already exists, throw the custom exception
+	            throw new DuplicateConflictException("Customer ID " + id + " already exists.");
+	        }
+
+	        // Performing other validations here using javax.validation annotations
+	        // Perform validations using the customValidationMethods
+	        customValidationMethods.validateName(customer.getName());
+	        customValidationMethods.validateEmail(customer.getEmail());
+	        customValidationMethods.validateAge(customer.getAge());
+	        customValidationMethods.validateCity(customer.getCity());
+	        customValidationMethods.validateState(customer.getState());
+	        customValidationMethods.validateCountry(customer.getCountry());
+	        customValidationMethods.validateZipcode(customer.getZipcode());
+	        customValidationMethods.validateOnboardedDate(customer.getOnboardedDate());
+	        
+	        // If all validations pass, save the customer
+	        customerRepository.save(customer);
+	        return "Success";
+	    }
+
 
 	@Override
 	public String updateCustomer(Customer customer) {
-		customerRepository.save(customer);
-		return "Success";
-	}
+		//customerRepository.save(customer);
+		//return "Success";
+		 String id = customer.getId();
+	        Optional<Customer> existingCustomerOptional = customerRepository.findById(id);
+
+	        if (existingCustomerOptional.isPresent()) {
+	            // Customer with the given ID exists, so perform the update
+	            customerRepository.save(customer);
+	            return "Success";
+	        } else {
+	            // Customer with the given ID does not exist, throw the custom exception
+	            throw new CustomerNotFoundException("Customer with ID " + id + " not found for update");
+	        }
+	    }
+	
 
 @Override
 	public String deleteCustomer(String id) {
+//		customerRepository.deleteById(id);
+//		return "Success";
+	Optional<Customer> existingCustomerOptional = customerRepository.findById(id);
+	if (existingCustomerOptional.isPresent()) {
+        // Customer with the given ID exists, so perform the delete
 		customerRepository.deleteById(id);
-		return "Success";
+        return "Success";
+    } else {
+        // Customer with the given ID does not exist, throw the custom exception
+        throw new CustomerNotFoundException("Customer with ID " + id + " not found for deletion");
+    }
+	
 	}
     
     //search
+@Override
+public List<Customer> searchCriteriaCustomer(SearchCriteria searchCriteria) {
+    String id = searchCriteria.getId();
+    String name = searchCriteria.getName();
+    Date onboardedDate = searchCriteria.getOnboardedDate();
+
+    // Implement the logic to search customers based on the provided criteria
+    // Use the customerRepository methods accordingly
+   
+    return customerRepository.searchCustomersByCriteria(id, name, onboardedDate);
+
+}
 
     @Override
     public List<Customer> searchCustomer(String query) {
@@ -93,16 +155,5 @@ public class CustomerServiceImple implements CustomerService {
 
 
 
-	
-
-
-	
-
-
-
-	
-
-	
-    
 	
 }
